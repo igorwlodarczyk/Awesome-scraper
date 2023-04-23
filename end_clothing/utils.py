644175cc26a00a394a47ff63ee0c7acd9546ddfc
Igor Wlodarczyk <wlodar.igor@gmail.com>
2023-04-23 21:35:34 +0200

    pep8 refractor

--- end_clothing/utils.py
+++ end_clothing/utils.py
@@ -1,6 +1,8 @@
 def parse_sizes(sizes):
     """
-    This function takes an argument called 'sizes' and checks whether it is a list or not. If 'sizes' is not a list, it is converted to a list with the original value as its only element. The function then returns the resulting list.
+    This function takes an argument called 'sizes' and checks whether it is a list or not.
+    If 'sizes' is not a list, it is converted to a list with the original value as its only element.
+    The function then returns the resulting list.
     :param sizes: string or a list object
     :return: list of strings
     """
